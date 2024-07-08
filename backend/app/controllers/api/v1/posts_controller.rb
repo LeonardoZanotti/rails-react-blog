56d399c990caf28c07ff5f51aea222396d000a8f
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2024-07-08 13:31:21 -0300

    added image handling in the backend api

--- backend/app/controllers/api/v1/posts_controller.rb
+++ backend/app/controllers/api/v1/posts_controller.rb
@@ -1,51 +1,63 @@
 class Api::V1::PostsController < ApplicationController
-  before_action :set_post, only: %i[ show update destroy ]
+	before_action :set_post, only: %i[ show update destroy ]
 
-  # GET /posts
-  def index
-    @posts = Post.order(created_at: :desc)
+	# GET /posts
+	def index
+		@posts = Post.order(created_at: :desc)
 
-    render json: @posts
-  end
+		posts_with_images = @posts.map do |post|
+			if post.image.attached?
+				post.as_json.merge(image_image: url_for(post.image))
+			else
+				post.as_json.merge(image_image: nil)
+			end
+		end
 
-  # GET /posts/1
-  def show
-    render json: @post
-  end
+		render json: posts_with_images
+	end
 
-  # POST /posts
-  def create
-    @post = Post.new(post_params)
+	# GET /posts/1
+	def show
+			if @post.image.attached?
+				render json: @post.as_json.merge(image_url: url_for(@post.image))
+			else
+				render json: @post.as_json.merge(image_url: nil)
+			end
+	end
 
-    if @post.save
-      render json: @post, status: :created, location: api_v1_post_url(@post)
-    else
-      render json: @post.errors, status: :unprocessable_entity
-    end
-  end
+	# POST /posts
+	def create
+		@post = Post.new(post_params)
 
-  # PATCH/PUT /posts/1
-  def update
-    if @post.update(post_params)
-      render json: @post
-    else
-      render json: @post.errors, status: :unprocessable_entity
-    end
-  end
+		if @post.save
+			render json: @post, status: :created, location: api_v1_post_url(@post)
+		else
+			render json: @post.errors, status: :unprocessable_entity
+		end
+	end
 
-  # DELETE /posts/1
-  def destroy
-    @post.destroy!
-  end
+	# PATCH/PUT /posts/1
+	def update
+		if @post.update(post_params)
+			render json: @post
+		else
+			render json: @post.errors, status: :unprocessable_entity
+		end
+	end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+	# DELETE /posts/1
+	def destroy
+		@post.destroy!
+	end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:title, :body)
-    end
+	private
+		# Use callbacks to share common setup or constraints between actions.
+		def set_post
+			@post = Post.find(params[:id])
+		end
+
+		# Only allow a list of trusted parameters through.
+		def post_params
+			params.require(:post).permit(:title, :body, :image)
+		end
 end
